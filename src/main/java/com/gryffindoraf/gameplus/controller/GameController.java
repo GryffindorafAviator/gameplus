a6228ed4fa17e6820236155de25894bac7e47a0f
gryffindoraf <phynew0111@gmail.com>
2024-02-11 20:44:16 -0500

    Modified Game class.

--- src/main/java/com/gryffindoraf/gameplus/controller/GameController.java
+++ src/main/java/com/gryffindoraf/gameplus/controller/GameController.java
@@ -1,5 +1,7 @@
 package com.gryffindoraf.gameplus.controller;
 
+import com.gryffindoraf.gameplus.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +12,9 @@ import java.io.IOException;
 
 @Controller
 public class GameController {
+    @Autowired
+    private GameService gameService;
+
     @RequestMapping(value = "game", method = RequestMethod.GET)
     public void getGame(@RequestParam(value = "game_name", required = false) String gameName, HttpServletResponse response) throws IOException {
 
