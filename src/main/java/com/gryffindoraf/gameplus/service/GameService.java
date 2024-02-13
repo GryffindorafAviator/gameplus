14cfa22317db798f2bef37de3880d74970d7ec11
gryffindoraf <phynew0111@gmail.com>
2024-02-12 23:35:39 -0500

    Modified Game class.

--- src/main/java/com/gryffindoraf/gameplus/service/GameService.java
+++ src/main/java/com/gryffindoraf/gameplus/service/GameService.java
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
+    private static final String TOKEN = ;
+    private static final String CLIENT_ID = ;
+    private static final String
+    private static final String
+    private static final String
 }
---
+++ src/main/java/com/gryffindoraf/gameplus/service/TwitchException.java
@@ -0,0 +1,9 @@
+package com.gryffindoraf.gameplus.service;
+
+public class TwitchException extends RuntimeException {
+    public TwitchException(String errorMessage) {
+        super(errorMessage);
+    }
+
+
+}
