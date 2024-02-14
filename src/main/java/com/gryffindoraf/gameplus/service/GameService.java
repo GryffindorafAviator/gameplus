0f546555a9e770ca08cb6494c6c7621e078a708c
gryffindoraf <phynew0111@gmail.com>
2024-02-13 22:43:27 -0500

    Modified Game class.

--- src/main/java/com/gryffindoraf/gameplus/service/GameService.java
+++ src/main/java/com/gryffindoraf/gameplus/service/GameService.java
@@ -1,12 +1,74 @@
 package com.gryffindoraf.gameplus.service;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 @Service
 public class GameService {
-    private static final String TOKEN = ;
-    private static final String CLIENT_ID = ;
-    private static final String
-    private static final String
-    private static final String
+    private static final String TOKEN = "Bearer hidzmbpam2mefaioafooznh37543wm";
+    private static final String CLIENT_ID = "zdb9dh0kzudmtc2boulg814lnk9zjr";
+    private static final String TOP_GAME_URL = "https://api.twitch.tv/helix/games/top?first=%s";
+    private static final String GAME_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/games?name=%s";
+    private static final int DEFAULT_GAME_LIMIT = 20;
+
+    private  String buildGameURL(String url, String gameName, int limit) {
+        if (gameName.equals("")) {
+            return String.format(url, limit);
+        }
+        else {
+            try {
+                gameName = URLEncoder.encode(gameName, "UTF-8");
+            }
+            catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return String.format(url, gameName);
+        }
+    }
+
+    private String searchTwitch(String url) throws TwitchException {
+        CloseableHttpClient httplient = HttpClients.createDefault();
+        ResponseHandler<String> responseHandler = response -> {
+            int responseCode = response.getStatusLine().getStatusCode();
+            if (responseCode != 200) {
+                System.out.println("Response status: " + response.getStatusLine().getReasonPhrase());
+                throw new TwitchException("Failed to get result from Twitch APT");
+            }
+            HttpEntity entity = response.getEntity();
+            if (entity == null) {
+                throw new TwitchException("Failed to get result from Twitch API");
+            }
+            JSONObject obj = new JSONObject(EntityUtils.toString(entity));
+            return obj.getJSONArray("data").toString();
+        };
+        try {
+            HttpGet request = new HttpGet(url);
+            request.setHeader("Authorization", TOKEN);
+            request.setHeader("Client-Id", CLIENT_ID);
+            return httplient.execute(request, responseHandler);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new TwitchException("Failed to get result from Twitch API");
+        }
+        finally {
+            try {
+                httplient.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
