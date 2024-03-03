6c20f67b42bb32ab1a353177a9066b43b904eb46
gryffindoraf <phynew0111@gmail.com>
2024-03-02 21:31:46 -0500

    Modified Game class.

---
+++ src/main/java/com/gryffindoraf/gameplus/controller/FavoriteController.java
@@ -0,0 +1,9 @@
+package com.gryffindoraf.gameplus.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FavoriteController {
+//    @Autowired
+}
