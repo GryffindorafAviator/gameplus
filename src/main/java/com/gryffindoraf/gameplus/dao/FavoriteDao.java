5fa265dfe3bb18ad4924e3f5464040530cb85e15
gryffindoraf <phynew0111@gmail.com>
2024-03-03 21:37:42 -0500

    Modified Game class.

--- src/main/java/com/gryffindoraf/gameplus/dao/FavoriteDao.java
+++ src/main/java/com/gryffindoraf/gameplus/dao/FavoriteDao.java
@@ -1,14 +1,14 @@
 package com.gryffindoraf.gameplus.dao;
 
 import com.gryffindoraf.gameplus.entity.db.Item;
+import com.gryffindoraf.gameplus.entity.db.ItemType;
 import com.gryffindoraf.gameplus.entity.db.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Repository
 public class FavoriteDao {
@@ -67,6 +67,39 @@ public class FavoriteDao {
             ex.printStackTrace();
         }
         return  new HashSet<>();
+    }
+
+    public Set<String> getFavoriteItemIds(String userId) {
+        Set<String> itemIds = new HashSet<>();
+
+        try (Session session = sessionFactory.openSession()) {
+            Set<Item> items = session.get(User.class, userId).getItemSet();
+            for (Item item : items) {
+                itemIds.add(item.getId());
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return itemIds;
+    }
+
+    public Map<String, List<String>> getFavoriteGameIds(Set<String> favoriteItemIds) {
+        Map<String, List<String>> itemMap = new HashMap<>();
+        for (ItemType type : ItemType.values()) {
+            itemMap.put(type.toString(), new ArrayList<>());
+        }
+
+        try (Session session = sessionFactory.openSession()) {
+            for (String itemId : favoriteItemIds) {
+                Item item = session.get(Item.class, itemId);
+                itemMap.get(item.getType().toString()).add(item.getGameId());
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return itemMap;
     }
 }
 
